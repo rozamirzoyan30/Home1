a6338006fbc825cd01d1a53204b78174307bfdb8
rozamirzoyan30 <rozamirzoyan30@gmail.com>
2022-12-22 19:46:59 +0400

    Add link's file and java's file

---
+++ Homework 2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	    public static void main(String[] args) {
+		            String a = "Hello ";
+			            String c = "5 ";
+
+				            System.out.println(a + "world");
+					            System.out.println(c + "раз");
+						        }
+}
